d3b0b5bac97c1b3db229b5c57e38eaba72b5e244
hique023 <pedroviski10@hotmail.com>
2022-04-15 15:21:42 -0300

    Adicionado nova rota para recuperar sessões de um analista

--- app.js
+++ app.js
@@ -26,23 +26,43 @@ app.use(cors());
 const dia = String(data.getDate()).padStart(2, "0");
 var mes = String(data.getMonth() + 1).padStart(2, "0");
 var ano = data.getFullYear();
+var hour = data.getHours(); // 0-23
+var minutes = data.getMinutes(); // 0-59
+var seconds = data.getSeconds();
 var dataAtual = ano + "-" + mes + "-" + dia;
-console.log("Ano: " + typeof ano + " " + ano);
-console.log("Mes: " + typeof mes + " " + mes);
-console.log("Dia: " + typeof dia + " " + dia);
-console.log(dataAtual);
+var horaAtual = hour + ":" + minutes + ":" + seconds;
+// console.log("Ano: " + typeof ano + " " + ano);
+// console.log("Mes: " + typeof mes + " " + mes);
+// console.log("Dia: " + typeof dia + " " + dia);
+// console.log(dataAtual);
+// console.log(horaAtual);
 
 // Dados da requisição
 const keyRequest = process.env.APP_ID;
 const dateFilter = `${ano}-${mes}-${dia}`;
 
-const base_url = `https://api.us0.swi-rc.com/integration/get_history_api.php?KEY=${keyRequest}&VARS=DATE_START DATE_END TECH_NAME TECH_USERNAME&FILTER_DATE_START_INI=${dateFilter}&FILTER_PROCESSED=ANSWERED&OUTPUT_FORMAT=JSON`;
+const base_url = `https://api.us0.swi-rc.com/integration/get_history_api.php?KEY=${keyRequest}&VARS=ID DATE_START DATE_END TECH_NAME TECH_USERNAME&FILTER_DATE_START_INI=${dateFilter}&FILTER_PROCESSED=ANSWERED&OUTPUT_FORMAT=JSON`;
+// const base_urltech = `https://api.us0.swi-rc.com/integration/get_history_api.php?KEY=${keyRequest}&VARS=ID DATE_START DATE_END TECH_NAME TECH_USERNAME&FILTER_DATE_START_INI=${dateFilter}&FILTER_PROCESSED=ANSWERED&FILTER_TECH_USERNAME=${req.params.tech}&OUTPUT_FORMAT=JSON`;
 
 // Rota GET para recuperar dados do MSP
 app.get("/msp", async (req, res) => {
   try {
     // Response é a resposta do Axios mas eu desestruturo de dentro do response
     const { data } = await axios(base_url);
+
+    return res.json(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Rota GET para recuperar dados do MSP de um analista específico
+app.get("/msptech/:tech", async (req, res) => {
+  try {
+    // Response é a resposta do Axios mas eu desestruturo de dentro do response
+    const { data } = await axios(
+      `https://api.us0.swi-rc.com/integration/get_history_api.php?KEY=${keyRequest}&VARS=ID DATE_START DATE_END TECH_NAME TECH_USERNAME&FILTER_DATE_START_INI=${dateFilter}&FILTER_PROCESSED=ANSWERED&FILTER_TECH_USERNAME=${req.params.tech}&OUTPUT_FORMAT=JSON`
+    );
 
     return res.json(data);
   } catch (error) {
